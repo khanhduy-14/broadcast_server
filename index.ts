3b8230fe59821db91aacc76926b7173b6800fd61
khanhduy-14 <tranhakhanhduy.dev@gmail.com>
2025-01-07 23:50:25 +0700

    feat (websocket): implement on upgrade event

--- index.ts
+++ index.ts
@@ -1,4 +1,27 @@
-const log = (text: string): void => {
-    console.log(text);
-}
-log('Hello World!');
+import {createServer} from 'http';
+
+const PORT = 1337
+
+const server = createServer((req, res) => {
+    res.writeHead(200);
+    res.end('Hello World!');
+}).listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
+
+
+server.on('upgrade', (req, socket, head) => {
+    console.log(`Upgrading ${req.url}`);
+    console.log({
+        head: req.headers,
+    })
+})
+
+;[
+    "uncaughtException",
+    "unhandledRejection"
+].forEach(event =>
+    process.on(event, (err) => {
+        console.error(`Error: ${event}, Message: ${err.stack || err}`)
+    })
+)
