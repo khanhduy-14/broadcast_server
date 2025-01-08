9aecc4c328c7ce9ff448eaf66dcfca01b9054466
khanhduy-14 <tranhakhanhduy.dev@gmail.com>
2025-01-08 23:50:17 +0700

    feat (websocket): establishing the handshake

--- index.ts
+++ index.ts
@@ -1,7 +1,36 @@
 import {createServer} from 'http';
+import crypto from 'crypto';
+import {IncomingMessage} from "node:http";
+import {Duplex} from "node:stream";
+import * as buffer from "node:buffer";
 
 const PORT = 1337
 
+const WEBSOCKET_MAGIC_STRING = '258EAFA5-E914-47DA-95CA-C5AB0DC85B11'
+
+function createSocketAccept (id : string){
+    const hash = crypto.createHash('sha1');
+    hash.update(id+ WEBSOCKET_MAGIC_STRING);
+    return hash.digest('base64');
+}
+
+function prepareHandshakeResponse (id: string){
+    const acceptKey = createSocketAccept(id);
+    return [
+        'HTTP/1.1 101 Switching Protocols',
+        'Upgrade: websocket',
+        'Connection: Upgrade',
+        `sec-webSocket-accept: ${acceptKey}`,
+        ''
+    ].map(line => line.concat('\r\n')).join('')
+}
+
+function onSocketUpgrade  (req: IncomingMessage, socket: Duplex, head: Buffer)  {
+    const  { 'sec-websocket-key': clientSocketKey} = req.headers
+    const response = prepareHandshakeResponse(clientSocketKey)
+    socket.write(response);
+
+}
 const server = createServer((req, res) => {
     res.writeHead(200);
     res.end('Hello World!');
@@ -10,12 +39,7 @@ const server = createServer((req, res) => {
 });
 
 
-server.on('upgrade', (req, socket, head) => {
-    console.log(`Upgrading ${req.url}`);
-    console.log({
-        head: req.headers,
-    })
-})
+server.on('upgrade', onSocketUpgrade)
 
 ;[
     "uncaughtException",
